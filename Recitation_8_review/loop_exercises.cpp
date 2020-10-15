8a7ac91e857ad82a5a8841ecf8556098a38a9d7b
Nancy Fulda <nfulda@byu.edu>
2020-10-15 09:55:50 -0600

    Create loop_exercises.cpp

---
+++ Recitation_8_review/loop_exercises.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+
+
+    // What does this do?
+    /*while (1) {
+        //do nothing
+    }*/
+
+
+
+
+    // How about this?
+    /*int i = 10;
+    while(i>0) {
+        cout << i << endl;
+        i--;
+    }*/
+
+
+
+
+    // And this?
+    /*int i = 10;
+    while(i>0) {
+        cout << i << endl;
+        i++;
+    }*/
+
+
+
+    // TODO: use a loop to print out all numbers between 0 and 100
+   
+    
+    
+    // TODO: use a loop to print all numbers between 0 and 100 that are divisible by 3
+  
+  
+  
+   
+    // An very weird loop example that employs randomness.
+    // Try to figure out what it does.
+    /*srand(time(0));
+    int i;
+    for(i=rand()%150; i>= 0 and i<=100; i = rand()%150) {
+        cout << i << endl;
+    }
+    cout << i << endl;*/
+
+
+
+
+    // Example: Using a loop to reverse the characters in a string
+    string myString = "";
+    cin >> myString;
+
+    int i=0;
+    int strLen =myString.length();
+    string myNewString = "";
+    while(i<strLen) {
+        myNewString += myString.at((strLen-1)-i);
+        i++;
+    }
+    cout << myNewString << endl;
+
+
+
+    return 0;
+}
